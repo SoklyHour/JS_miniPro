5811933c5524aa955873bd2dd77c032a5143b46f
Sokly Hour <hoursokly011@gmail.com>
2024-09-25 14:16:14 -0500

    add

--- weather/weather.js
+++ weather/weather.js
@@ -7,6 +7,14 @@ const weather = {
         const randomTemperature = Math.floor(Math.random()*35); // ramdom temperature between 0 to 35
         const conditions = ["Sunny", 'Cloudy', "Rainy", "Snowy", "Windy"];
         const randomCondition = [Math.floor(Math.random() * conditions.length)]
-        
+
+        // Update the object with new values
+        this.Temperature = randomTemperature;
+        this.conditions = randomCondition;
+
+        return `Weather updated: ${this.city}, ${this.Temperature}°C, ${this.Condition}`;
+
     }
 }
+
+
